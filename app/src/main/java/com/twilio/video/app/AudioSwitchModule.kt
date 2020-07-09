4a68798bfb0d6dff76caf1bbd4e95abc2734a709
John Qualls <jaltonqualls@twilio.com>
2020-07-09 16:05:36 -0600

    Support for multiple bt devices progress [skip ci]

--- app/src/main/java/com/twilio/video/app/AudioSwitchModule.kt
+++ app/src/main/java/com/twilio/video/app/AudioSwitchModule.kt
@@ -1,14 +1,19 @@
 package com.twilio.video.app
 
 import android.app.Application
+import android.media.AudioManager
+import android.media.AudioManager.OnAudioFocusChangeListener
 import com.twilio.audioswitch.AudioSwitch
 import dagger.Module
 import dagger.Provides
+import timber.log.Timber
 
 @Module(includes = [ ApplicationModule::class ])
 class AudioSwitchModule {
 
     @Provides
     fun providesAudioSwitch(application: Application): AudioSwitch =
-            AudioSwitch(application)
+            AudioSwitch(application, true, OnAudioFocusChangeListener { i ->
+                Timber.d("Audio focus changed to: $i")
+            })
 }
